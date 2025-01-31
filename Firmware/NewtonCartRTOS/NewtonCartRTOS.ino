f9f9d9177b304a0812cbdef0f70fa243f4cab332
Peter Salan <55043123+petersalan@users.noreply.github.com>
2025-01-30 22:40:46 -0600

    FinalVersion(2024)
    Se suben archivos finales para construcción de NewtonCart

--- Firmware/NewtonCartRTOS/NewtonCartRTOS.ino
+++ Firmware/NewtonCartRTOS/NewtonCartRTOS.ino
@@ -1,40 +1,55 @@
+/*
+Universidad de San Carlos de Guatemala, Facultad de Ingenieria 
+Escuela de Ingeniería Mecanica Electrica, Peter Alexander Salán Aparicio
+Programación de plataforma "Newton-Cart" 
+Microcontrolador: Chip ESP-WROOM-32, montado en PCB NewtonCart v1.0
+Sensores y modulos: HC-SR04; TAL220 con XFW-HX011; HC-020K, ; GY-571, BH1750; TP4056.
+Alimentacion: 5V y 3.3V 
+*/
+//Librerías
 #include "HX711.h"
 #include "Wire.h"
+#include <BLE2902.h>
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <BLEUtils.h>
-#include <BLE2902.h>
 #include <BLEAdvertisedDevice.h>
 #include <BLEScan.h>
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
 
-//:::::::::::::::::::: DEFINITIONS ::::::::::::::::::::::::
+//:::::::::::::::::::: DEFINICIONES ::::::::::::::::::::::::
 float f;
 float d;
 float v;
 float MPUValues[6];
-float acc, pitch;
+float accx, accy, accz, pitch, corr_pitch, roll, corr_roll, yaw, corr_yaw;
 float distance;
+float distance_a;
+String valores;
 long tempo;
-//----------- RESERVE IO'S IN PCB --------------
+int16_t sampling_freq = 200;
+
+//----------- I/O's RESERVADOS EN MICROCONTROLADOR Y PCB --------------
 #define RESERVA_1 32
 #define RESERVA_2 33
 #define RESERVA_3 34
 
-//----------- RTOS TASK DEFINITION --------------
+//----------- DEFINICIÓN DE TAREA RTOS --------------
 TaskHandle_t Sens, Comm;
 SemaphoreHandle_t syncro;
 
-//----------- BLE --------------
+//----------- Definición BLE --------------
 BLECharacteristic* BLEDistance;
 BLECharacteristic* BLEForce;
 BLECharacteristic* BLEVelocity;
 BLECharacteristic* BLEAcceleration;
 BLECharacteristic* BLEPitch;
-bool deviceConnected = false;  //Connection active or not.
+bool deviceConnected = false;  //Compara si conexión activa.
 float distanceValue = 0;
 float forceValue = 0;
 
-#define SERVICE_UUID            "0000180D-0000-1000-8000-00805F9B34FB"
+#define SERVICE_UUID            "00001819-0000-1000-8000-00805F9B34FB" //Location and navigation service 1819 (Utilizado por similitud).
 #define CHARACTERISTIC_DISTANCE_UUID  "00002713-0000-1000-8000-00805F9B34FB"
 #define CHARACTERISTIC_FORCE_UUID  "00002714-0000-1000-8000-00805F9B34FB"
 #define CHARACTERISTIC_VELOCITY_UUID  "00002715-0000-1000-8000-00805F9B34FB"
@@ -48,15 +63,15 @@ class MyServerCallbacks: public BLEServerCallbacks {
 
   void onDisconnect(BLEServer* pServer){
     deviceConnected = false;
-    }                                     //Returns a true or false in "deviceConnected" variable
+    }                                     //Retorna un valor de falso o verdadero en "deviceConnected" 
 };
 
-//----------- SENSORS --------------
+//----------- Definiciones SENSORES --------------
 //LOADCELL
 #define DT 18
 #define SCK 19
 HX711 scale;
-float calibration = +227700; // Calibration value calculated for specific used loadcell.
+float calibration = 256500/1000; // Valor de calibración calculado para la celda específica en gramos. 227700 previo calibración de prueba de peso.
 
 //HC-SR04
 #define TRIGGER 2
@@ -66,9 +81,9 @@ float calibration = +227700; // Calibration value calculated for specific used l
 #define MPU_SDA 15
 #define MPU_SCL 13
 #define MPU 0x68
-#define A_R 16384.0 // To obtain acceleration in terms of 1G, 32768/2
+#define A_R 16384.0 // Obtener valor de aceleración en términos de 1G, 32768/2
 #define G_R 131.0 // 32768/250
-#define RadtoDeg = 57.295779 //MPU Conversion values
+#define RadtoDeg = 57.295779 //Valor de conversión para MPU.
 int16_t AcX, AcY, AcZ, GyX, GyY, GyZ;
 float Acc[3];
 float Gy[3];
@@ -77,10 +92,10 @@ float values[6];
 long prev_time;
 float dt;
 
-//OPTICAL ENCODER
+//ENCODER OPTOELECTRÓNICO
 #define PULSE 34
-#define CHANGES 39 //Qty of changes in the used encoder.
-#define DIAMETER 70 //Diameter of wheel in wich is located the encoder, in mm.
+#define CHANGES 40 //# Cambios de estado en el encoder óptico.
+#define DIAMETER 64 // Diámetro del neumático colocado, en mm.
 long prev_time2 = 0;
 long dt2 = 0;
 int pulsecount = 0;
@@ -88,29 +103,29 @@ float velo;
 
 
 
-//::::::::::::::::::::: FUNCTIONS ::::::::::::::::::::::::::
+//::::::::::::::::::::: FUNCIONES ::::::::::::::::::::::::::
 //----------- BLE --------------
 void ConfigBLE(){
-  BLEDevice::init("ESP32PET"); //Create the BLE Device
-
+  BLEDevice::init("ESP32PET"); // Se crea un servicio de BLE.
+  
   BLEServer *pServer = BLEDevice::createServer(); 
-  pServer->setCallbacks(new MyServerCallbacks()); //Creating the BLE Server
+  pServer->setCallbacks(new MyServerCallbacks()); //Se genera el servidor de BLE.
 
-  BLEService *pService = pServer->createService(SERVICE_UUID); //Create the BLE Service
+  BLEService *pService = pServer->createService(SERVICE_UUID); //Se crea el servicio BLE
 
   BLEDistance = pService->createCharacteristic(            
                       CHARACTERISTIC_DISTANCE_UUID,
-                      BLECharacteristic::PROPERTY_NOTIFY   //Create a Distance Characteristic
+                      BLECharacteristic::PROPERTY_NOTIFY   //Se define la característica Distancia.
                       );
   
   BLEForce = pService->createCharacteristic(
                       CHARACTERISTIC_FORCE_UUID,
-                      BLECharacteristic::PROPERTY_NOTIFY   //Create a Force Characteristic
+                      BLECharacteristic::PROPERTY_NOTIFY   //Se define la característica Fuerza.
                       );
 
   BLEVelocity = pService->createCharacteristic(
                       CHARACTERISTIC_VELOCITY_UUID,
-                      BLECharacteristic::PROPERTY_NOTIFY   //Create a Force Characteristic
+                      BLECharacteristic::PROPERTY_NOTIFY   //Se define la característica Velocidad.
                       );
 
   BLEAcceleration = pService->createCharacteristic(
@@ -120,24 +135,24 @@ void ConfigBLE(){
 
   BLEPitch = pService->createCharacteristic(
                       CHARACTERISTIC_PITCH_UUID,
-                      BLECharacteristic::PROPERTY_NOTIFY   //Create a Force Characteristic
+                      BLECharacteristic::PROPERTY_NOTIFY   //Se define la característica Pitch
                       );
                                            
   BLEDistance->addDescriptor(new BLE2902());
   BLEForce->addDescriptor(new BLE2902());
   BLEVelocity->addDescriptor(new BLE2902());
   BLEAcceleration->addDescriptor(new BLE2902());
-  BLEPitch->addDescriptor(new BLE2902());    //BLE2902 needed to notify
+  BLEPitch->addDescriptor(new BLE2902());    //Notificaciones parte de  BLE2902
   
-  pService->start();   //Start the service
+  pService->start();   //Se da inicio al servicio descrito 
 
-  pServer->getAdvertising()->start();  //Start advertising
+  pServer->getAdvertising()->start();  //Se comienza a notificar.
   Serial.println("Waiting for a client connection to notify...");  
   }
 
 //----------- LOADCELL READ --------------
 float GetForce(){
-  scale.set_scale(calibration);
+  //scale.set_scale(calibration);
   float force = (scale.get_units());
   return force;
 }
@@ -149,20 +164,24 @@ float GetDistance(){
   digitalWrite(TRIGGER, HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIGGER, LOW);
-  tempo = pulseIn(ECHO, HIGH); //tempo takes the duration (in ms) of the echo pin, timeout 60ms
-  distance = tempo*0.345/2; // distance equal to sound velocity * time /2
+  tempo = pulseIn(ECHO, HIGH); //Tempo toma la duración hacia Echo pin, con timeout de 60s.
+  distance_a = distance;
+  if(abs(distance - distance_a) >= (1.0*sampling_freq))
+    distance = distance_a;
+  else distance = tempo*0.345/2; // Distancia equivale a velocidad por tiempo /2
+  
   return distance;
 }
-//----------- IMU READ --------------
+//----------- IMU MPU6050 READ --------------
 float GetIMU(){
   //Leer los valores del Acelerometro de la IMU
    Wire.beginTransmission(MPU);
    Wire.write(0x3B); //Pedir el registro 0x3B - corresponde al AcX
    Wire.endTransmission(false);
    Wire.requestFrom(MPU,6,true);   //A partir del 0x3B, se piden 6 registros
-   AcX=Wire.read()<<8|Wire.read(); //Cada valor ocupa 2 registros
-   AcY=Wire.read()<<8|Wire.read();
-   AcZ=Wire.read()<<8|Wire.read();
+   AcX=(Wire.read()<<8|Wire.read()); //Cada valor ocupa 2 registros
+   AcY=(Wire.read()<<8|Wire.read()) - 24000;
+   AcZ=(Wire.read()<<8|Wire.read()) - 4200;
  
    //A partir de los valores del acelerometro, se calculan los angulos Y, X
    //respectivamente, con la formula de la tangente.
@@ -194,58 +213,78 @@ float GetIMU(){
    Angle[2] = Angle[2]+Gy[2]*dt;
  
    //Mostrar los valores por consola
-   String valuesString = "90, " +String(Angle[0]) + "," + String(Angle[1]) + "," + String(Angle[2]) + ", -90";
+   //String valuesString = "90, " +String(Angle[0]) + "," + String(Angle[1]) + "," + String(Angle[2]) + ", -90";
+   //Serial.println(valuesString);
+   //Serial.println(dt);
    MPUValues[0]= AcX/A_R;
-   MPUValues[1]= AcY;
+   MPUValues[1]= AcY/A_R;
    MPUValues[2]= AcZ/A_R; //Valores de aceleración obtenidos por IMU
-   MPUValues[3]= Angle[0]; //ROLL
-   MPUValues[4]= Acc[1]; //PITCH
-   MPUValues[5]= Angle[2]; //YAW
+   MPUValues[3]= Angle[0] - (corr_roll); //ROLL
+   MPUValues[4]= Angle[1] - (corr_pitch); //PITCH
+   MPUValues[5]= Angle[2] - (corr_yaw); //YAW
+   //MPUValues[2]= dt;
    
    return 0;
 }   
 //----------- OPTICAL ENCODER READ -----------
 float GetVelocity(){
   dt2 = millis()-prev_time2;
-  if(dt2>1000){
-    velo = ((2*PI)/CHANGES)*pulsecount; //Arroja velocidad estimada en m/s
+  if(dt2>=sampling_freq){
+    velo = ((2*PI)*(pulsecount/40.0) * DIAMETER/2) * (1000/sampling_freq) ; //Arroja velocidad estimada en m/s
+    //Serial.print("Velocity: ");
+    //Serial.print(velo);
+    //Serial.print(" --pulsos: ");
+    //Serial.println(pulsecount);
     pulsecount=0;
     prev_time2 = millis();
     }
+
   return velo;
 }
-//----------- INTERRUPTIONS -----------  
+//----------- INTERRUPCIONES -----------  
 void IRAM_ATTR EncoderCounter(){
   pulsecount++;
   }
 
-//-----------********* RTOS **********--------------
+//-----------********* DEFINICIÓN RTOS **********--------------
 void Sensing(void* parameters){
   for(;;){
       //Serial.println("Entering Sensing");
       xSemaphoreTake(syncro, portMAX_DELAY);
-      //f = GetForce();  
-      //d = GetDistance();
-      //v = GetVelocity();
+      f = GetForce();  
+      d = GetDistance();
+      v = GetVelocity();
       GetIMU();
-      acc = MPUValues[0];
+      accx = MPUValues[0];
+      //accy = MPUValues[1];
+      //accy = MPUValues[2];
+      //roll = MPUValues[3];
       pitch = MPUValues[4];
-      //Serial.print("Fuerza: ");
-      //Serial.println(f);
+      //yaw = MPUValues[5];
+      Serial.print("Fuerza: ");
+      Serial.println(f);
       //Serial.print("Distancia: ");
       //Serial.println(d);
-      Serial.println("Euler :");
-      //Serial.println("AccX =" + String(MPUValues[0]));
-      //Serial.println("AccZ =" + String(MPUValues[2]));
-      Serial.println("PITCH =" + String(MPUValues[4]));
-      //Serial.print("Velocity: ");
-      //Serial.print(v);
-      //Serial.print(" --pulsos: ");
-      //Serial.println(pulsecount);
+      //Serial.println("Euler :");
+
+      /*Serial.print(90.0);
+      Serial.print(" ");
+      Serial.print(accx);
+      Serial.print(" ");
+      Serial.print(accy);
+      Serial.print(" ");
+      Serial.print(accz);
+      Serial.print(" ");
+      Serial.print(-90.0);
+      Serial.println();*/
+
+      valores = "90, " + String(roll) + "," + String(pitch) + "," + String(yaw) + ", -90";
+      //Serial.println(valores);
+      
       //delay(500);
-      Serial.println("Sensing completed");
+      //Serial.println("Sensing completed");
       xSemaphoreGive(syncro); 
-      delay(1000);   
+      delay(sampling_freq);   
     } 
   }
 
@@ -262,43 +301,44 @@ void Communicate(void* parameters){
         dtostrf(d,1,2,distanceString);
         dtostrf(f,1,2,forceString);
         dtostrf(v,1,2,velocityString);
-        dtostrf(acc,1,2,accelString);
+        dtostrf(accx,1,2,accelString);
         dtostrf(pitch,1,2,pitchString);
         
-        //Setting the value to the characteristic
+        //Se asigna un valor a la característica
         BLEDistance->setValue(distanceString);
         BLEForce->setValue(forceString);
         BLEVelocity->setValue(velocityString);
-        BLEAcceleration->setValue(accelString);
-        BLEPitch->setValue(pitchString);
+        BLEAcceleration->setValue(pitchString);
+        BLEPitch->setValue(accelString);
         
-        //Notifying the connected client
+        //Se notifica sobre la conexión
         BLEDistance->notify();
         BLEForce->notify();
         BLEVelocity->notify();
         BLEAcceleration->notify();
         BLEPitch->notify();
-        Serial.println("Sent distance: " + String(distanceString));
+        //Serial.println("Sent distance: " + String(distanceString));
         Serial.println("Sent force: " + String(forceString));
-        Serial.println("Sent velocity: " + String(velocityString));
-        Serial.println("Sent acceleration: " + String(accelString));;
-        Serial.println("Sent pitch angle: " + String(pitchString));
-        Serial.println();
+        //Serial.println("Sent velocity: " + String(velocityString));
+        //Serial.println("Sent acceleration: " + String(accelString));;
+        //Serial.println("Sent pitch angle: " + String(pitchString));
+        Serial.println("enviado");
       }
       xSemaphoreGive(syncro);
-      delay(1000);
+      delay(sampling_freq);
     }
   }
 
   
-//:::::::::::::::::: MAIN LOOPS ::::::::::::::::::::
+//:::::::::::::::::: MAIN ::::::::::::::::::::
 void setup() {
+  //WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //Deshabilitar brownout detector
   delay(3000);
   Serial.begin(115200);
   //----------- LOADCELL -----------
   scale.begin(DT,SCK);
-  scale.set_scale();
-  scale.tare(); //Reset scale to 0.
+  scale.set_scale(calibration);
+  scale.tare(); //Se da reset a la báscula
   long zero_factor = scale.read_average();
   Serial.print("Zero factor: ");
   Serial.println(zero_factor);
@@ -311,33 +351,38 @@ void setup() {
   Wire.write(0x6B);
   Wire.write(0);
   Wire.endTransmission(true);
+  GetIMU(); // Llamado a la rutina de lectura de MPU para correccion de ceros iniciales.
+  corr_roll = MPUValues[3];
+  corr_pitch = MPUValues[4];
+  corr_yaw = MPUValues[5];
+  
   //----------- OPTICAL ENCODER -----------
   pinMode(PULSE, INPUT);
   attachInterrupt(PULSE, EncoderCounter, CHANGE);
-  //----------- COMMUNICATION CONFIG -----------
+  //----------- COMM CONFIG -----------
   ConfigBLE();
   delay(1000);
-  //----------- RTOS TASK SETUP -----------
+  //----------- SETUP TAREAS RTOS -----------
   syncro = xSemaphoreCreateMutex();
   xTaskCreatePinnedToCore(
-    Sensing,            //Call to function.
+    Sensing,            //Llamada a la función
     "Sensing Task",
     8000,
     NULL,
     1,
-    &Sens,      //Task definition in TaskHandler_t function
-    1);         //Core
+    &Sens,      //Como se define la tarea en función TaskHandler_t
+    1);         //Selección core
   delay(500);
   xTaskCreatePinnedToCore(
-    Communicate,            //Call to function.
+    Communicate,            //Llamada a función
     "Communication Task",
     8000,
     NULL,
     2,
-    &Comm,      //Task definition in TaskHandler_t function
-    0);         //Core
+    &Comm,      //Como se define la tarea en función TaskHandler_t
+    0);         //Selección core
 }
 
 void loop() {
-  
+//No se ejecuta.
 }
